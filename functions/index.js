73bc1a9ccc899fa665dce3a47c228fdd1a28bb6e
tujii <inthusan@hotmail.de>
2026-07-14 14:25:12 +0200

    feat: register trackEvent Cloud Function

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,9 @@ admin.initializeApp();
 const { validatePDFUpload } = require('./validatePDF');
 const { scheduledCleanup }  = require('./scheduledCleanup');
 const { menuCard }          = require('./menuCard');
+const { trackEvent }        = require('./trackEvent');
 
 exports.validatePDFUpload = validatePDFUpload;
 exports.scheduledCleanup  = scheduledCleanup;
 exports.menuCard          = menuCard;
+exports.trackEvent       = trackEvent;
